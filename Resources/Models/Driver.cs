4cd157265b6c39b01ae30ef15130c5241f1ae1b4
jsonta <56021506+jsonta@users.noreply.github.com>
2020-04-07 01:44:47 +0200

    Dzialajaca baza danych kierowcow

--- Resources/Models/Driver.cs
+++ Resources/Models/Driver.cs
@@ -2,8 +2,34 @@
 {
     public class Driver
     {
-        public int Id { get; set; }
+        public long? Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string BirthDay { get; set; }
+        public long? PhoneNmbr { get; set; }
+        public string Email { get; set; }
+        public string AddrStrtName { get; set; }
+        public int? AddrBldngNmbr { get; set; }
+        public int? AddrFlatNmbr { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+
+        public bool HasIntValue(string varName)
+        {
+            switch (varName)
+            {
+                case "Id":
+                    return Id.HasValue;
+                case "PhoneNmbr":
+                    return PhoneNmbr.HasValue;
+                case "AddrBldngNmbr":
+                    return AddrBldngNmbr.HasValue;
+                case "AddrFlatNmbr":
+                    return AddrFlatNmbr.HasValue;
+                default:
+                    break;
+            }
+            return false;
+        }
     }
 }
